bdbfc81f9ebdc1d65c7f81fa9444b72523196771
Ruslan <mstrbsc@gmail.com>
2019-02-02 22:08:31 +0200

    Add test pin

--- src/main.c
+++ src/main.c
@@ -10,11 +10,47 @@
 
 
 #include "stm32f10x.h"
-			
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+
+GPIO_InitTypeDef GPIO_InitStruct;
+
 
 int main(void)
 {
+	volatile uint32_t pause;
 
-	for(;;);
+	RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOC, ENABLE);
+
+	GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_11 | GPIO_Pin_12;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	for(;;)
+	{
+
+		GPIO_SetBits(GPIOC, GPIO_Pin_11);
+		pause=0x00ff;
+		while (pause--);
+		GPIO_SetBits(GPIOC, GPIO_Pin_12);
+		pause=0x00ff;
+		while (pause--);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_11);
+		pause=0x00ff;
+		while (pause--);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_12);
+		pause=0x00ffffff;
+		while (pause--);
+
+	}
 
 }
+
+
+
+
+
+
+
+
